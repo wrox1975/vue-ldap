3ecce3bc6b914768a048392cda99eae2852dceac
maciejzabl <165154944+maciejzabl@users.noreply.github.com>
2025-02-04 14:12:24 +0100

    Maciej adjustments
    - changed node imports to type module
    - added cors without options
    - added route folder
    - removed endpoint from index.js
    - fixed login endpoint
    - adjusted endpoint path in Login.vue

--- api/index.js
+++ api/index.js
@@ -1,40 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ldap = require('ldapjs');
-const dotenv = require('dotenv');
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { userRoutes } from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
-const ldapClient = ldap.createClient({
-  url: process.env.LDAP_URL,
-});
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  const searchOptions = {
-    scope: 'sub',
-    filter: `(sAMAccountName=${username})`
-  };
-
-  ldapClient.search(process.env.LDAP_SEARCH_BASE, searchOptions, (err, searchRes) => {
-    if (err) return res.status(500).send(err);
-
-    searchRes.on('searchEntry', (entry) => {
-      const dn = entry.object.dn;
-      ldapClient.bind(dn, password, (err) => {
-        if (err) return res.status(401).send('Invalid credentials');
-        res.send('Login successful');
-      });
-    });
-
-    searchRes.on('end', (result) => {
-      if (result.status !== 0) return res.status(500).send(result);
-    });
-  });
-});
+// routes
+app.use("/api/user", userRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
